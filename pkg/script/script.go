7ba2f6e10b4f3e3bcdcebef78f4aefe6a09189b6
yago-123 <me@yago.ninja>
2024-06-23 09:05:22 +0300

    script: add structure to the implementation

--- pkg/script/script.go
+++ pkg/script/script.go
@@ -1,42 +1,108 @@
 package script
 
-import "fmt"
+import "strings"
+
+type ScriptType uint
 
 // Script types
 const (
 	// P2PK = Pay-to-PubKey
-	P2PK = iota
+	P2PK ScriptType = iota
 	// P2PKH = Pay-to-PubKey-Hash
 	// P2PKH
 
 	// ...
 )
 
-type ScriptOperator uint
+type Script []ScriptElement
+type ScriptElement uint
 
-// Operators
 const (
-	OP_CHECKSIG ScriptOperator = iota
+	// Special elements
+	PUB_KEY ScriptElement = iota
+
+	// Operators
+	OP_CHECKSIG
+
+	UNDEFINED
 )
 
 var operatorNames = [...]string{
+	"PUB_KEY",
 	"OP_CHECKSIG",
+
+	"UNDEFINED",
 }
 
-func (op ScriptOperator) String() string {
-	if op >= 0 && op < ScriptOperator(len(operatorNames)) {
-		return operatorNames[op]
+func (op ScriptElement) OutsideBoundaries() bool {
+	return op >= ScriptElement(len(operatorNames)-1)
+}
+
+func (op ScriptElement) IsSpecialCase() bool {
+	// todo() extend with more other special cases
+	return op >= PUB_KEY && op <= PUB_KEY
+}
+
+func (op ScriptElement) String() string {
+	if op >= ScriptElement(len(operatorNames)) {
+		// return UNDEFINED element
+		return operatorNames[len(operatorNames)-1]
 	}
 
-	return "OP_UNKNOWN"
+	return operatorNames[op]
 }
 
-func NewScript(scriptType uint, pubKey []byte) string {
+func NewScript(scriptType ScriptType, pubKey []byte) string {
+	script := Script{UNDEFINED}
+
+	// if there is no public key, return undefined directly
+	if len(pubKey) == 0 {
+		return UNDEFINED.String()
+	}
+
+	// generate script based on type
 	switch scriptType {
 	case P2PK:
-		return fmt.Sprintf("%s %s", pubKey, OP_CHECKSIG.String())
-	default:
-		return "UNKNOWN"
+		script = Script{PUB_KEY, OP_CHECKSIG}
+		break
 	}
 
+	// todo() the render will switch to a hex string eventually
+	// render script to string
+	return script.String(pubKey)
+}
+
+func (s Script) String(pubKey []byte) string {
+	rendered := []string{}
+
+	for _, element := range s {
+		toRender := ""
+
+		if element.OutsideBoundaries() {
+			return UNDEFINED.String()
+		}
+
+		// render special cases
+		if element.IsSpecialCase() {
+			switch element {
+			case PUB_KEY:
+				toRender = string(pubKey)
+				break
+			}
+		}
+
+		// render operators
+		if !element.IsSpecialCase() {
+			toRender = element.String()
+		}
+
+		// if no element has been rendered, return UNDEFINED
+		if toRender == "" {
+			return UNDEFINED.String()
+		}
+
+		rendered = append(rendered, toRender)
+	}
+
+	return strings.Join(rendered, " ")
 }
--- pkg/script/script_test.go
+++ pkg/script/script_test.go
@@ -1,1 +1,25 @@
 package script
+
+import "testing"
+
+func TestNewScript(t *testing.T) {
+	type args struct {
+		scriptType ScriptType
+		pubKey     []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"check regular script generation for P2PK", args{scriptType: P2PK, pubKey: []byte("public-key")}, "public-key OP_CHECKSIG"},
+		{"generation with empty public key", args{scriptType: P2PK, pubKey: []byte{}}, UNDEFINED.String()},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewScript(tt.args.scriptType, tt.args.pubKey); got != tt.want {
+				t.Errorf("NewScript() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
