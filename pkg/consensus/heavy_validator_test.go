0511bdb6c7bf12ba6aebb88a813ecbed5fbbfbb9
yago-123 <me@yago.ninja>
2024-06-30 10:21:44 +0300

    add testing methods

--- pkg/consensus/heavy_validator_test.go
+++ pkg/consensus/heavy_validator_test.go
@@ -1,1 +1,26 @@
 package consensus //nolint:testpackage // don't create separate package for tests
+import "testing"
+
+func TestHValidator_validateInputRemainUnspent(t *testing.T) {
+
+}
+
+func TestHValidator_validateBalance(t *testing.T) {
+
+}
+
+func TestHValidator_validateOwnershipOfInputs(t *testing.T) {
+
+}
+
+func TestHValidator_validateNumberOfCoinbaseTxs(t *testing.T) {
+
+}
+
+func TestHValidator_validateNoDoubleSpendingInsideBlock(t *testing.T) {
+
+}
+
+func TestHValidator_validateBlockHash(t *testing.T) {
+
+}
--- pkg/consensus/light_validator_test.go
+++ pkg/consensus/light_validator_test.go
@@ -1,1 +1,7 @@
 package consensus //nolint:testpackage // don't create separate package for tests
+
+import "testing"
+
+func TestLValidator_validateInputsDontMatch(t *testing.T) {
+
+}
