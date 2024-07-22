6e5c75a8b437e24eeacb66ee1f15c08e477eb605
yago-123 <me@yago.ninja>
2024-07-22 12:59:49 +0200

    pow: modify test

--- pkg/consensus/miner/pow_test.go
+++ pkg/consensus/miner/pow_test.go
@@ -16,6 +16,7 @@ func TestProofOfWork_CalculateBlockHash(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, nonce > 0)
 	assert.Equal(t, []byte{0x0}, hash[:1])
+	assert.NotEqual(t, []byte{0x0}, hash[1:2])
 
 	bh = kernel.NewBlockHeader([]byte("1"), 1, []byte("merkle-root"), 1, []byte("prev-block-hash"), 16, 0)
 	pow = NewProofOfWork(ctx, bh)
@@ -23,4 +24,5 @@ func TestProofOfWork_CalculateBlockHash(t *testing.T) {
 	assert.NoError(t, err)
 	assert.True(t, nonce > 0)
 	assert.Equal(t, []byte{0x0, 0x0}, hash[:2])
+	assert.NotEqual(t, []byte{0x0}, hash[2:3])
 }
