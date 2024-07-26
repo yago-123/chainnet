23b54dd960d70386608a56076cc30f0e50e8fa36
yago-123 <me@yago.ninja>
2024-07-26 19:06:25 +0200

    chain: add bloom filter

---
+++ pkg/chain/bloom/bloom.go
@@ -0,0 +1,37 @@
+package bloom
+
+import (
+	"chainnet/pkg/kernel"
+	"encoding/binary"
+	"fmt"
+)
+
+const BloomFilterSize = 32
+
+type BlockBloomFilter struct {
+	bloom map[string][]bool
+}
+
+func NewBlockBloomFilter() *BlockBloomFilter {
+	return &BlockBloomFilter{
+		bloom: make(map[string][]bool),
+	}
+}
+
+func (bf *BlockBloomFilter) AddBlock(block *kernel.Block) {
+	bf.bloom[string(block.Hash)] = make([]bool, BloomFilterSize)
+
+	for _, tx := range block.Transactions {
+		index := binary.BigEndian.Uint64(tx.ID) % BloomFilterSize
+		bf.bloom[string(block.Hash)][index] = true
+	}
+}
+
+func (bf *BlockBloomFilter) PresentInBlock(txID, blockID []byte) (bool, error) {
+	if filter, ok := bf.bloom[string(blockID)]; ok {
+		index := binary.BigEndian.Uint64(txID) % BloomFilterSize
+		return filter[index], nil
+	}
+
+	return false, fmt.Errorf("block %s not found in bloom filter", string(blockID))
+}
