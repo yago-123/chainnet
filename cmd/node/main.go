b1d8502130d5e532d4cd80566fad41ea3abe87e1
Yago <yago.gou@proton.me>
2024-08-07 20:19:21 +0300

    Add Helm charts (#23)
    * helm: add initial structure
    
    * helm: chart.yaml mod
    
    * helm: add templates for miner and node statefulsets
    
    * helm: adjust values and README
    
    ---------
    
    Co-authored-by: yago-123 <yfernandezgou@gmail.com>

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,30 +1,34 @@
 package main
 
+import "time"
+
 const (
 	DifficultyPoW = 4
 	MaxNoncePoW   = 100
 )
 
 func main() {
-	/*
-		baseURL := "localhost:8080"
-
-		cfg := config.NewConfig(logrus.New(), DifficultyPoW, MaxNoncePoW, baseURL)
-		// Initialize your blockchain and other components
-		bolt, err := storage.NewBoltDB("_fixture/chainnet-store", "chainnet-block-bucket", "chainnet-header-bucket", encoding.NewGobEncoder())
-		if err != nil {
-			cfg.Logger.Errorf("Failed to initialize BoltDB: %s", err)
-		}
-
-		// create blockchain
-		// bc := blockchain.NewBlockchain(cfg, miner.NewProofOfWork(cfg.DifficultyPoW, hash.NewSHA256()), bolt, &mockConsensus.MockHeavyValidator{})
-
-		cfg.Logger.Infof("Server listening on %s", cfg.BaseURL)
-
-		explorer := explorer.NewExplorer(bolt)
-		err = http.ListenAndServe(cfg.BaseURL, NewHTTPRouter(explorer)) //nolint:gosec // add timeout later
-		if err != nil {
-			cfg.Logger.Fatalf("Failed to start server: %s", err)
-		}
-	*/
+	time.Sleep(1 * time.Hour)
 }
+
+/*
+	baseURL := "localhost:8080"
+
+	cfg := config.NewConfig(logrus.New(), DifficultyPoW, MaxNoncePoW, baseURL)
+	// Initialize your blockchain and other components
+	bolt, err := storage.NewBoltDB("_fixture/chainnet-store", "chainnet-block-bucket", "chainnet-header-bucket", encoding.NewGobEncoder())
+	if err != nil {
+		cfg.Logger.Errorf("Failed to initialize BoltDB: %s", err)
+	}
+
+	// create blockchain
+	// bc := blockchain.NewBlockchain(cfg, miner.NewProofOfWork(cfg.DifficultyPoW, hash.NewSHA256()), bolt, &mockConsensus.MockHeavyValidator{})
+
+	cfg.Logger.Infof("Server listening on %s", cfg.BaseURL)
+
+	explorer := explorer.NewExplorer(bolt)
+	err = http.ListenAndServe(cfg.BaseURL, NewHTTPRouter(explorer)) //nolint:gosec // add timeout later
+	if err != nil {
+		cfg.Logger.Fatalf("Failed to start server: %s", err)
+	}
+*/
