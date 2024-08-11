796b9d07f9c3e8249b0fc57edf7627905abcc8cd
yago-123 <yfernandezgou@gmail.com>
2024-08-11 22:24:17 +0300

    linter

--- pkg/chain/p2p/p2p_timeout.go
+++ pkg/chain/p2p/p2p_timeout.go
@@ -3,11 +3,12 @@ package p2p
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
-	"time"
 )
 
 // TimeoutStream wraps a network.Stream with read and write timeouts
