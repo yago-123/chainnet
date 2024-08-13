3396ed79be7956822d5ebb80664be6fce15412fb
yago-123 <me@yago.ninja>
2024-08-13 18:22:35 +0300

    storage: add test for checking ErrNotFound condition

---
+++ pkg/storage/boltdb_test.go
@@ -0,0 +1,34 @@
+package storage //nolint:testpackage // don't create separate package for tests
+
+import (
+	"chainnet/pkg/encoding"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const MockStorageFile = "test-file"
+
+func TestBoltDB_NotFound(t *testing.T) {
+	defer os.Remove(MockStorageFile)
+
+	bolt, err := NewBoltDB(MockStorageFile, "block-bucket", "header-bucket", encoding.NewGobEncoder())
+	require.NoError(t, err)
+
+	_, err = bolt.GetLastBlock()
+	assert.Equal(t, ErrNotFound, err)
+
+	_, err = bolt.GetGenesisBlock()
+	assert.Equal(t, ErrNotFound, err)
+
+	_, err = bolt.GetGenesisHeader()
+	assert.Equal(t, ErrNotFound, err)
+
+	_, err = bolt.RetrieveBlockByHash([]byte(""))
+	assert.Equal(t, ErrNotFound, err)
+
+	_, err = bolt.RetrieveHeaderByHash([]byte(""))
+	assert.Equal(t, ErrNotFound, err)
+}
