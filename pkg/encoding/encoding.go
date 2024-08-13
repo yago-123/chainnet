c52f6488a0a36b5d87f6da3ba259accaad6ed01e
yago-123 <yfernandezgou@gmail.com>
2024-08-13 16:03:26 +0300

    encoding: add new methods to interface

--- pkg/encoding/encoding.go
+++ pkg/encoding/encoding.go
@@ -7,9 +7,11 @@ import (
 type Encoding interface {
 	SerializeBlock(b kernel.Block) ([]byte, error)
 	SerializeHeader(bh kernel.BlockHeader) ([]byte, error)
+	SerializeHeaders(bhs []*kernel.BlockHeader) ([]byte, error)
 	SerializeTransaction(tx kernel.Transaction) ([]byte, error)
 
 	DeserializeBlock(data []byte) (*kernel.Block, error)
 	DeserializeHeader(data []byte) (*kernel.BlockHeader, error)
+	DeserializeHeaders(data []byte) ([]*kernel.BlockHeader, error)
 	DeserializeTransaction(data []byte) (*kernel.Transaction, error)
 }
