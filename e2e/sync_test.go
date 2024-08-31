0d4d41c59caf2ab375a6691f1312aa4ab087ffa8
yago-123 <yfernandezgou@gmail.com>
2024-09-01 02:06:01 +0300

    e2e: add build tag

--- e2e/sync_test.go
+++ e2e/sync_test.go
@@ -1,3 +1,6 @@
+//go:build e2e
+// +build e2e
+
 package e2e
 
 import (
@@ -10,10 +13,11 @@ func TestNodeSyncDuringStartup(t *testing.T) {
 	minikube := orchestrator.NewMinikube(os.Stdout, os.Stderr)
 	// defer minikube.Delete()
 
-	client, err := minikube.Create("v1.31.0", 1, 5, 5120)
+	_, err := minikube.Create("v1.31.0", 1, 5, 5120)
 	if err != nil {
 		t.Errorf("unable to create minikube cluster: %s", err)
 	}
+	defer minikube.Delete()
 
-	client.DeployWithHelm()
+	// client.DeployWithHelm()
 }
