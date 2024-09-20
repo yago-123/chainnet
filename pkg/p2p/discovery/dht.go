64d6faea2347ba4b07b7c3dffab4a9b77962cad4
yago-123 <yfernandezgou@gmail.com>
2024-09-20 14:39:43 +0300

    remove debug func

--- pkg/p2p/discovery/dht.go
+++ pkg/p2p/discovery/dht.go
@@ -8,8 +8,6 @@ import (
 	ds "github.com/ipfs/go-datastore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/sirupsen/logrus"
-	"time"
 )
 
 type DHTDiscovery struct {
@@ -20,9 +18,6 @@ type DHTDiscovery struct {
 func NewDHTDiscovery(cfg *config.Config, host host.Host, netSubject observer.NetSubject) (*DHTDiscovery, error) {
 	// todo(): consider adding persistent data store
 	d := dht.NewDHT(context.Background(), host, ds.NewMapDatastore())
-
-	// todo(): add function for watching peerstore from host in order to monitor while developing
-	go printPeersDiscovered(host, cfg.Logger)
 
 	return &DHTDiscovery{
 		dht:      d,
@@ -56,19 +51,4 @@ func (d *DHTDiscovery) Stop() error {
 
 	d.isActive = false
 	return nil
-}
-
-func printPeersDiscovered(h host.Host, logger *logrus.Logger) {
-	ticker := time.NewTicker(20 * time.Second)
-
-	for {
-		select {
-		case <-ticker.C:
-			logger.Errorf("Peers discovered: %d", len(h.Peerstore().Peers()))
-			for _, p := range h.Peerstore().Peers() {
-				logger.Errorf("- peer %s", p.String())
-			}
-		}
-	}
-
 }
