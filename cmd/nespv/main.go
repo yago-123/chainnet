428bba325bb9f28cd76b447a368b5ca5323e6ae4
yago-123 <yfernandezgou@gmail.com>
2024-10-20 23:02:58 +0200

    wallet: remove unused declaration

--- cmd/nespv/main.go
+++ cmd/nespv/main.go
@@ -7,42 +7,4 @@ import (
 
 func main() {
 	cmd.Execute(logrus.New())
-	/*
-		cmd.Execute(logrus.New())
-
-		// todo() remove this cfg initialization
-		cfg = config.NewConfig()
-
-		cfg.Logger.SetLevel(logrus.DebugLevel)
-
-		cfg.Logger.Infof("starting wallet with config %v", cfg)
-
-		// general consensus hasher (tx, block hashes...)
-		consensusHasherType := hash.SHA256
-
-		// general consensus signer (tx)
-		consensusSigner := crypto.NewHashedSignature(
-			sign.NewECDSASignature(),
-			hash.NewSHA256(),
-		)
-
-		// algorithm required for wallet hashing
-		walletHasher := crypto.NewMultiHash(
-			[]hash.Hashing{hash.NewSHA256(), hash.NewRipemd160()},
-		)
-
-		// create new wallet for storing mining rewards
-		_, err := wallet.NewWallet(
-			cfg,
-			[]byte("1"),
-			validator.NewLightValidator(hash.GetHasher(consensusHasherType)),
-			consensusSigner,
-			walletHasher,
-			hash.GetHasher(consensusHasherType),
-			encoding.NewProtobufEncoder(),
-		)
-		if err != nil {
-			cfg.Logger.Fatalf("Error creating new wallet: %s", err)
-		}
-	*/
 }
