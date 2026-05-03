5dda2c91655120df78366db5ce09a28ecd79fc41
Yago <git@yago.ninja>
2026-05-03 19:11:32 +0200

    Move CLI to SDK methods

--- cmd/cli/cmd/tx.go
+++ cmd/cli/cmd/tx.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/spf13/cobra"
+	sdkv1beta "github.com/yago-123/chainnet-sdk-go/v1beta"
 	"github.com/yago-123/chainnet/config"
 	sdkv1beta "github.com/yago-123/chainnet/pkg/sdk/v1beta"
 )
@@ -39,8 +40,7 @@ var listTxsCmd = &cobra.Command{
 
 		// print transactions
 		for _, tx := range txs {
-
-			logger.Infof("{\n%s}\n", tx.String())
+			logger.Infof("{\n%+v}\n", tx)
 		}
 	},
 }
